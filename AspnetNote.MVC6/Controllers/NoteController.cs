1e167efad46b8c89d35b65ed2c540aed57d8c8a8
여왕벌 <s730921@hotmail.com>
2019-07-18 11:15:31 +0900

    게시판 조회/등록 기능

---
+++ AspnetNote.MVC6/Controllers/NoteController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetNote.MVC6.DataContext;
+using AspnetNote.MVC6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspnetNote.MVC6.Controllers
+{
+    public class NoteController : Controller
+    {
+        /// <summary>
+        /// 게시물 리스트
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            using (var db = new AspnetNoteDbContext())
+            {
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+        }
+
+        /// <summary>
+        /// 게시물 추가
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Note model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspnetNoteDbContext())
+                {
+                    db.Notes.Add(model);
+
+                    if (db.SaveChanges() > 0)
+                    {
+                        //return RedirectToAction("Index", "Note"); //다른 폴더로 이동할 때
+                        return Redirect("Index");                   // 같은 폴더에서 이동할 때
+                    }
+                    ModelState.AddModelError(string.Empty, "게시물을 저장할 수 없습니다.");
+                }
+            }
+            return View(model);
+        }
+
+        /// <summary>
+        /// 게시물 수정
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 삭제
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
